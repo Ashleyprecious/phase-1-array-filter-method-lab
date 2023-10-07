52f3f54b1d35ff5e87d8bab5503f002d88cec22b
Ashley <ashley.precios@student.moringaschool.com>
2023-10-07 15:51:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(drivers,string) {
+    const newDriversList=drivers.filter(driver=> string.toLowerCase()===driver.toLowerCase());
+    return newDriversList;
+}
+console.log(newDriversList);
+
+function fuzzyMatch(drivers,string){
+    const driverLenght=string.length;
+    const newDriversList=drivers.filter(driver=> string.toLowerCase()===driver.toLowerCase().substr(0,driverLenght));
+    return newDriversList;
+}
+console.log(newDriversList)
+
+function matchName(drivers,name) { // name to the driver
+    const newDriversList=drivers.filter(driver=> driver.name.toLowerCase()===name.toLowerCase());
+    return newDriversList;
+
+}
+console.log(matchName)
